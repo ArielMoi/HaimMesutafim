e87959f797a2a741e67798716956ef40ca10ee7b
Netta Avidan <nettavidan19@gmail.com>
2021-04-27 19:56:31 +0300

    -

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,6 +1,11 @@
 
 import React, { useState } from 'react'
+<<<<<<< Updated upstream
 import 'axios' from 'axios'
+=======
+import axios from 'axios';
+import CurrentVol from './components/CurrentVol'
+>>>>>>> Stashed changes
 
 function Input() {
     const [search, setSearch] = useState('')
@@ -20,8 +25,17 @@ function Input() {
 
     return (
         <div className="Input-contianer">
+<<<<<<< Updated upstream
              <input type="text" placeholder="Enter volunteer link here" onChange={volunteer => { setSearch(volunteer.target.value)}}/>
              <input type="button" value="search" onClick={getNewQuery}/>
+=======
+            <input type="text" placeholder="Enter volunteer link here" onChange={volunteer => { setSearch(volunteer.target.value) }} />
+            <input type="button" value="search" onClick={getUrl} />
+
+            <div>
+                <CurrentVol data={data} key={data.id}/>
+            </div>
+>>>>>>> Stashed changes
         </div>
     )
 }
