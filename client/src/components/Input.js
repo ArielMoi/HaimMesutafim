5c00073fa3cc21d3d872023e199e639240cbca4c
Netta Avidan <nettavidan19@gmail.com>
2021-04-27 18:04:45 +0300

    up

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,13 +1,27 @@
 
 import React, { useState } from 'react'
+import 'axios' from 'axios'
 
 function Input() {
     const [search, setSearch] = useState('')
+    
+    const getUrl = async() =>{
+        try{
+            const data = await axios.get(`${search}`)
+            setRequest(data.data.hits)
+        } catch(e){
+             console.log('error')
+        }
+     }
+ 
+     useEffect(() =>{
+        getUrl()
+     }, [search])
 
     return (
         <div className="Input-contianer">
              <input type="text" placeholder="Enter volunteer link here" onChange={volunteer => { setSearch(volunteer.target.value)}}/>
-            <input type="submit">Search</input>
+             <input type="button" value="search" onClick={getNewQuery}/>
         </div>
     )
 }
