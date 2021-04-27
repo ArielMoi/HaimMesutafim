552d431a9eb4e1e893066e2ef19dd5f45441abb7
foad alhsnat <foadal0548@gmail.com>
2021-04-27 18:57:12 +0300

    tets

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import Box from './components/Box';
+import Input from './components/Input'
 import './app.css';
 
 function App() {
   return (
     <div className="App">
+      <Input/>
       <Router>
 
           <Nav/>
